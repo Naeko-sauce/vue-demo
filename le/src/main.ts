35be1e27d257c07e52dcd701e432269116034d58
Naeko-sauce <3443471283@qq.com>
2023-10-30 03:24:47 +0800

    src文件夹别名配置
    src文件夹别名配置

--- le/src/main.ts
+++ le/src/main.ts
@@ -1,4 +1,4 @@
-import App from "./App.vue";
+import App from "@/App.vue";
 import { createApp } from "vue";
 //引入element-puls样式
 import ElementPlus from "element-plus";
--- le/vite.config.ts
+++ le/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+//配置src的路径别名
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve("./src")//相对路径配置别名使用@代替src
+    }
+  }
 });
