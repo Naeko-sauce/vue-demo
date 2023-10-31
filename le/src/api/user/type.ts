bd6b1fe448081a48621f4a6f3ccfd7cde4317a96
Naeko-sauce <3443471283@qq.com>
2023-10-31 17:00:42 +0800

    添加统一管理接口
    添加了登陆接口方法和获取用户信息方法

---
+++ le/src/api/user/type.ts
@@ -0,0 +1,14 @@
+//登陆接口需要携带的参数ts类型
+export interface loginForm {
+    username: String,
+    password: String
+}
+//登陆接口返回数据类型
+interface dataType {
+    token: String
+}
+export interface loginResponseData {
+    code: number,
+    data: dataType
+}
+//定义服务器返回用户信息相关的数据类型
